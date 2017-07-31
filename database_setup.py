c8e9bf4ce22abe1d3d74f56aca6bd60887298f83
Sarah Caplan <caplan.sarah@gmail.com>
2017-07-31 08:08:05 -0700

    Update projec to final version

--- database_setup.py
+++ database_setup.py
@@ -2,14 +2,35 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
+
+class User(Base):
+  __tablename__ = 'user'
+
+  name = Column(String(80), nullable = False)
+  email = Column(String(80), nullable = False)
+  picture = Column(String(250))
+  id = Column(Integer, primary_key = True)
+
+
+  @property
+  def serialize(self):
+    """Return object data in easily serializeable format"""
+    return {
+      'name' : self.name,
+      'email' : self.email,
+      'picture' : self.picture,
+      'id'  : self.id,
+    }
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -18,7 +39,7 @@ class Restaurant(Base):
            'name'         : self.name,
            'id'           : self.id,
        }
- 
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
@@ -30,6 +51,8 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
@@ -44,8 +67,7 @@ class MenuItem(Base):
        }
 
 
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 
-engine = create_engine('sqlite:///restaurantmenu.db')
- 
 
 Base.metadata.create_all(engine)
